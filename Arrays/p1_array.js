fa6be310268110c3402408bec1fbb185485d4ed6
Devasya <devasyarajguru@gmail.com>
2023-01-24 14:00:11 +0530

    Array basics

---
+++ Arrays/p1_array.js
@@ -0,0 +1,23 @@
+let arr = [1,3,4,"Dev", true,false]; // Array can hold many and different values
+console.log(arr);
+
+// Accessing Array elements
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+
+// Length of an Array
+console.log(arr.length) // 6
+
+arr[0] = 9; // Array is mutable you can change values in it
+console.log(arr);
+
+console.log(typeof arr); // Array is an Object type
+
+// for(var i = 0; arr.length; i++)
+// {
+//     console.log(arr[i]);
+// }
