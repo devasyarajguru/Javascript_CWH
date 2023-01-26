769f5eea642ee8fa0d321105dbb8bf075946e0b9
Devasya <devasyarajguru@gmail.com>
2023-01-26 16:36:44 +0530

    Array Methods

---
+++ Array_methods/p2_array_methods.js
@@ -0,0 +1,14 @@
+// delete in Array
+let arr = [1,4,6,78,9,5];
+// console.log(arr.length);
+// delete arr[1];
+// console.log(arr);
+// console.log(arr.length);
+
+// Concat in Array
+let arr2 = [3,8,5,1,4];
+let arr3 = [13,18,15,11,14];
+
+let mergeArray = arr.concat(arr2);
+console.log(mergeArray);
+console.log(arr2.concat(arr,arr3))
